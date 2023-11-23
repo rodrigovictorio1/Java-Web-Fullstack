e5b696808f5b94814819e242234d09d99e2f6500
Rodrigo Victorio <Rodrigo Victorio@DESKTOP-CHFDQDL>
2023-11-23 19:55:37 -0300

    Primeiro commit

--- Formação_Java_Web_Fullstack/src/test/Main.java
+++ Formação_Java_Web_Fullstack/src/test/Main.java
@@ -1,5 +1,8 @@
 package test;
 
 public class Main {
-	System.out.println("Olá, mundo!");
+	public static void main(String[] args) {
+		System.out.println("Olá, mundo!");
+		System.out.println("Meu primeiro programa.");
+	}
 }
